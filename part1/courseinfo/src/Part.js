ac6f27c4018d7b84e50e0518053b33262b964438
Arnau <kenny.knp@gmail.com>
2020-04-16 21:20:49 +0200

    part 1 things

---
+++ part1/courseinfo/src/Part.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+export const Part = (props) => (
+    <p>
+        {props.part} {props.exes}
+    </p>
+)
---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+
+
+export const Total = (props) => (
+    <p>Number of exercises {props.total}</p>
+)
